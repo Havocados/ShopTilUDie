25fa01b4a7e721a41dde7e89e83d97f794e81853
Havocados <sebastian.t.skoglund@gmail.com>
2025-11-14 11:09:47 +0100

    fix: - adjust alignment and button positioning in home page hero section

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ export function homePageContent(
 
   // ----------------------------------- START OF INNER HTML FOR HOME PAGE  -----------------------------------
   target.innerHTML = /* html */ `
-  <div id="hero-bg" class="pb-4 pb-lg-0 text-center bg-dark text-white">
+  <div id="hero-bg" class="pb-4 pb-lg-0 text-center text-lg-start bg-dark text-white">
     <div class="container">
       <div class="row align-items-center g-5 py-0 px-5 vh-80" >
         <div class="col-10 col-sm-8 col-lg-6">
@@ -21,7 +21,7 @@ export function homePageContent(
             <p class="lead">
               ${heroData.subtitle}
             </p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <div class="d-grid gap-2 d-md-flex justify-content-center">
             <button type="button" 
                       class="btn btn-lg btn-hero px-4 me-md-2"
                       id="hero-cta-button">
